fcc0f98c1662a87874544e262b3aa730702dc77e
AJAY R <63496620+ajayravi238@users.noreply.github.com>
2024-12-04 20:05:22 +0530

    Create Amstrong

---
+++ Amstrong.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Amstrong {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+	    String num = sc.nextLine();
+	    int length = num.length();
+	    int sum = 0;
+	    for(int i=0; i<num.length(); i++) {
+	    	int a = Character.getNumericValue(num.charAt(i));
+	    	double pow = Math.pow(a, length);
+	    	sum += pow;
+	    }
+	    if(Integer.parseInt(num) == sum) {
+	    	System.out.println(num + " is Amstrong");
+	    }
+	    else {
+	    	System.out.println(num + " is not Amstrong");
+	    }
+	}
+
+}
