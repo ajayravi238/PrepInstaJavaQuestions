ab0cad30de86853dcefe9f4784d3f63856606c98
AJAY R <63496620+ajayravi238@users.noreply.github.com>
2024-12-05 22:36:44 +0530

    Create RemoveBracketFromAlgebricExpression

---
+++ String/RemoveBracketFromAlgebricExpression.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class RemoveBracketFromAlgebricExpression {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String str = sc.nextLine();
+		for(int i=0; i<str.length(); i++) {
+			if(str.charAt(i) == '{' || str.charAt(i) == '}' || str.charAt(i) == '(' || 
+					str.charAt(i) == ')' || str.charAt(i) == '[' || str.charAt(i) == ']') {
+				continue;
+			}
+			else {
+				System.out.print(str.charAt(i));
+			}
+		}
+	}
+
+}
