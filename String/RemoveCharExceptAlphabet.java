a81c4d37b4f7c83d7afe0a99835fddec1a61aa72
AJAY R <63496620+ajayravi238@users.noreply.github.com>
2024-12-05 22:37:18 +0530

    Create RemoveCharExceptAlphabet

---
+++ String/RemoveCharExceptAlphabet.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class RemoveCharExceptAlphabet {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String str = sc.nextLine();
+		for(int i=0; i<str.length(); i++) {
+			if((str.charAt(i) >= 'a' && str.charAt(i) <= 'z') || str.charAt(i) >= 'A' && str.charAt(i) <= 'Z') {
+				continue;
+			}
+			else {
+				System.out.print(str.charAt(i));
+			}
+		}
+	}
+
+}
