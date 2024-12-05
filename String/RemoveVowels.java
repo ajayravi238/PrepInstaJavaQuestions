c33c6bac66a873b0b3f77fc44c79be38fa9a28bb
AJAY R <63496620+ajayravi238@users.noreply.github.com>
2024-12-05 22:38:21 +0530

    Create RemoveVowels

---
+++ String/RemoveVowels.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class RemoveVowels {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String inp = sc.nextLine();
+		String str = " ";
+		str = inp.toLowerCase();
+		for(int i=0; i<str.length(); i++) {
+			if(str.charAt(i) == 'a' || str.charAt(i) == 'e' || str.charAt(i) == 'i' || 
+					str.charAt(i) == 'o' || str.charAt(i) == 'u') {
+				continue;
+			}
+			else {
+				System.out.print(str.charAt(i));
+			}
+		}
+	}
+
+}
