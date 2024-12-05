dfb68d1a45d2b5a8a668a8f8fe172b1753243c48
AJAY R <63496620+ajayravi238@users.noreply.github.com>
2024-12-05 22:37:45 +0530

    Create RemoveSpaceFromString

---
+++ String/RemoveSpaceFromString.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class RemoveSpaceFromString {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String str = sc.nextLine();
+		for(int i=0; i<str.length(); i++) {
+			if(str.charAt(i) != ' ') {
+				System.out.print(str.charAt(i));
+			}
+		}
+	}
+
+}
