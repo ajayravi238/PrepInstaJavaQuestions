e2eed2f97684cc53fb043f1b5faba2d3c0af53f9
AJAY R <63496620+ajayravi238@users.noreply.github.com>
2024-12-08 19:32:18 +0530

    Create Fibonacci Series Upto N

---
+++ FibonacciSeriesUptoN.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class FibonacciSeriesUptoN {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int num = sc.nextInt();
+		int first = 0, second = 1, fib = 0;
+		System.out.print(first + " " + second + " ");
+		for(int i=2; i<num; i++) {
+			fib = first + second;
+			System.out.print(fib + " ");
+			first = second;
+			second = fib;
+		}
+	}
+
+}
