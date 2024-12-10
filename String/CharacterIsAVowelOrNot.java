e4fbdbc839b1a77ea736829f224d6f1f323bf773
AJAY R <63496620+ajayravi238@users.noreply.github.com>
2024-12-10 13:15:24 +0530

    Create Character Is A Vowel Or Not

---
+++ String/CharacterIsAVowelOrNot.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class CharacterIsAVowelOrNot {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String str = sc.nextLine();
+		str = str.toLowerCase();
+		char ch = str.charAt(0);
+		if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+			System.out.println(ch + " is Vowel");
+		}
+		else {
+			System.out.println(ch + " is not a Vowel");
+		}
+	}
+
+}
