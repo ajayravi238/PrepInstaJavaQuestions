290f21725a8b928b4a5a6ec3369290b44b7b7dfe
AJAY R <63496620+ajayravi238@users.noreply.github.com>
2024-12-10 13:42:32 +0530

    Create Palindrome Or Not

---
+++ String/PalindromeOrNot.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class PalindromeOrNot {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String str = sc.nextLine();
+		String revstr = "";
+		for(int i=str.length()-1; i>=0; i--) {
+			revstr += str.charAt(i);
+		}
+		if(str.equals(revstr)) {
+			System.out.println("Palindrome");
+		}
+		else {
+			System.out.println("Not a palindrome");
+		}
+	}
+
+}
